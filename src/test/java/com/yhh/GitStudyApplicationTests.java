f6b8add6f4f67159ef1d1a7d9becda66fb86a646
jjx <875987070@qq.com>
2021-10-29 17:18:42 +0800

    123456789

---
+++ src/test/java/com/yhh/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
