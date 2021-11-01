9683b64981d366645c564eb963e0680e07c8553a
jjx <875987070@qq.com>
2021-11-01 11:36:13 +0800

    hot-fix2

--- src/main/java/com/yhh/GitStudyApplication.java
+++ src/main/java/com/yhh/GitStudyApplication.java
@@ -9,7 +9,7 @@ public class GitStudyApplication {
     public static void main(String[] args) {
         //SpringApplication.run(GitStudyApplication.class, args);
         System.out.println("12322");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix-test");
     }
 
 }
